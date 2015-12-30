a65785a9a9457c7a38836174aa01059476327f05
weicheng xi <weicheng.xi@newtouch.cn>
2015-12-30 20:29:33 +0800

    ...

--- test.js
+++ test.js
@@ -1,3 +1,70 @@
 // alert('message')
 
 console.log(Backbone.VERSION);
+
+var object = {};
+
+_.extend(object, Backbone.Events);
+
+object.on("alert", function(msg) {
+    console.log("Triggered " + msg);
+});
+
+object.trigger("alert", "an event");
+
+var dispatcher = _.clone(Backbone.Events);
+
+
+var Sidebar = Backbone.Model.extend({
+    promptColor: function() {
+        // var cssColor = prompt("Please enter a CSS color:");
+        var cssColor = 'red';
+        this.set({
+            color: cssColor
+        });
+    }
+});
+
+window.sidebar = new Sidebar;
+
+sidebar.on('change:color', function(model, color) {
+    $('body').css({
+        background: color
+    });
+});
+
+sidebar.set({
+    color: 'white'
+});
+
+sidebar.promptColor();
+
+
+
+var Note = Backbone.Model.extend({
+
+  initialize: function() { },
+
+  author: function() { },
+
+  coordinates: function() { },
+
+  allowedToEdit: function(account) {
+    return true;
+  }
+
+});
+
+var PrivateNote = Note.extend({
+
+  allowedToEdit: function(account) {
+    return account.owns(this);
+  }
+
+});
+
+var Note = Backbone.Model.extend({
+  set: function(attributes, options) {
+    Backbone.Model.prototype.set.apply(this, arguments);
+  }
+});
